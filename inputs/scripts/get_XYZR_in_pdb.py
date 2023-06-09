eb7ae340376060c727dcd6a8332c446a7dc55206
muskaan9jindal <muskaanjinda99@gmail.com>
2023-06-09 12:17:51 +0530

    updates input files

--- inputs/scripts/get_XYZR_in_pdb.py
+++ inputs/scripts/get_XYZR_in_pdb.py
@@ -15,17 +15,24 @@ def extract_calpha_with_coordinates(input_pdb_file, output_pdb_file):
                     calpha_atoms.append(residue['CA'])
 
     with open(output_pdb_file, 'w') as f:
+        count = 1
         for atom in calpha_atoms:
             coord = atom.get_coord()
             line = 'ATOM '
+            line += str(count) + ' '
             line += str(atom.get_id()) + ' '
             line += str(atom.get_parent().get_parent().get_id()) + ' '
             line += str(atom.get_parent().get_resname()) + ' '
+            line += str(atom.get_full_id()[3][1]) + ' '
             line += str(atom.get_coord()[0]) + ' '
             line += str(atom.get_coord()[1]) + ' '
             line += str(atom.get_coord()[2]) + ' '
             line += '2.8 \n'
             f.write(line)
+            count += 1
+            # res_num = atom.get_full_id()[3][1]
+            # print(res_num[3][1])
+
 
 
 input_file = sys.argv[1]
