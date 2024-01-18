3909be1eaee76cb1102c61b5bdea5e05ffa574b8
muskaan9jindal <muskaanjindal99@gmail.com>
2024-01-18 13:09:30 +0530

    ADD EDC crosslinks on acid residues

--- scripts/1_processing_jwalk_output.py
+++ scripts/1_processing_jwalk_output.py
@@ -6,20 +6,23 @@ import pandas as pd
 input_folder = sys.argv[1]
 
 for file in glob.glob(os.path.join(input_folder, '*.txt')):
-    df = pd.read_csv(file, delim_whitespace=True, skiprows=1, header=None)
-    df.columns = ["Index", "Model", "Atom1", "Atom2", "SASD"]
+    try:
+        df = pd.read_csv(file, delim_whitespace=True, skiprows=1, header=None)
+        df.columns = ["Index", "Model", "Atom1", "Atom2", "SASD"]
 
-    filtered_df = df[df["Atom1"].str.split('-').str[2] != df["Atom2"].str.split('-').str[2]]
-    df2 = pd.concat([
-        filtered_df["Atom1"].str.split('-').str[1].rename('res1'),
-        filtered_df["Atom1"].str.split('-').str[2].rename('prot1'),
-        filtered_df["Atom2"].str.split('-').str[1].rename('res2'),
-        filtered_df["Atom2"].str.split('-').str[2].rename('prot2'),
-    ], axis=1)
+        filtered_df = df[df["Atom1"].str.split('-').str[2] != df["Atom2"].str.split('-').str[2]]
+        df2 = pd.concat([
+            filtered_df["Atom1"].str.split('-').str[1].rename('res1'),
+            filtered_df["Atom1"].str.split('-').str[2].rename('prot1'),
+            filtered_df["Atom2"].str.split('-').str[1].rename('res2'),
+            filtered_df["Atom2"].str.split('-').str[2].rename('prot2'),
+        ], axis=1)
 
-    output_filename = os.path.splitext(os.path.basename(file))[0] + '_interprotein_crosslinks.csv'
-    output_path = os.path.join(input_folder, output_filename)
+        output_filename = os.path.splitext(os.path.basename(file))[0] + '_interprotein_crosslinks.csv'
+        output_path = os.path.join(input_folder, output_filename)
 
-    df2.to_csv(output_path, index=False)
-    print(df2)
-    df2.to_csv(f'{file}_crosslinks.csv', index = False)
+        df2.to_csv(output_path, index=False)
+
+    except pd.errors.EmptyDataError:
+        print(f"File {file} does not contain any data.")
+        continue
--- scripts/master_script.sh
+++ scripts/master_script.sh
@@ -3,48 +3,48 @@
 # for EDC
 #go to /home/muskaan/easal_benchmark_JWALK_results/JWALK_results_EDC/ directory and run the following:
 patterns=(
-    "1clv 1 EDC"
-    "1r0r 2 EDC"
+    "1clv 8 EDC"
+    "1r0r 6 EDC"
     "2ayo 5 EDC"
     "1kxp 7 EDC"
     "2hle 9 EDC"
     "1dfj 4 EDC"
-    "2b42 1 EDC"
+    "2b42 10 EDC"
 )
 
 # Iterate over the patterns and execute the Python script
 for pattern in "${patterns[@]}"; do
     set -- $pattern
-    pdb_file="${1}_crosslink_list_interprotein_crosslinks.csv"
+    pdb_file="${1}_interprotein.csv"
     python ~/EASAL/scripts/2_getting_random_xlinks.py "$pdb_file" "$2" "$3"
 done
 
 # for DSSO
 #go to /home/muskaan/easal_benchmark_JWALK_results/JWALK_results_DSSO/ directory and run the following:
-patterns=(
-    "1clv 2 DSSO"
-    "1clv 6 DSSO"
-    "1r0r 3 DSSO"
-    "1r0r 7 DSSO"
-    "2ayo 4 DSSO"
-    "2ayo 8 DSSO"
-    "2ayo 13 DSSO"
-    "1kxp 4 DSSO"
-    "1kxp 7 DSSO"
-    "1kxp 11 DSSO"
-    "2hle 5 DSSO"
-    "2hle 10 DSSO"
-    "2hle 14 DSSO"
-    "1dfj 3 DSSO"
-    "1dfj 9 DSSO"
-    "1dfj 12 DSSO"
-    "2b42 5 DSSO"
-    "2b42 10 DSSO"
-)
-
-# Iterate over the patterns and execute the Python script
-for pattern in "${patterns[@]}"; do
-    set -- $pattern
-    pdb_file="${1}_crosslink_list_interprotein_crosslinks.csv"
-    python ~/EASAL/scripts/2_getting_random_xlinks.py "$pdb_file" "$2" "$3"
-done
+# patterns=(
+#     "1clv 2 DSSO"
+#     "1clv 6 DSSO"
+#     "1r0r 3 DSSO"
+#     "1r0r 7 DSSO"
+#     "2ayo 4 DSSO"
+#     "2ayo 8 DSSO"
+#     "2ayo 13 DSSO"
+#     "1kxp 4 DSSO"
+#     "1kxp 7 DSSO"
+#     "1kxp 11 DSSO"
+#     "2hle 5 DSSO"
+#     "2hle 10 DSSO"
+#     "2hle 14 DSSO"
+#     "1dfj 3 DSSO"
+#     "1dfj 9 DSSO"
+#     "1dfj 12 DSSO"
+#     "2b42 5 DSSO"
+#     "2b42 10 DSSO"
+# )
+#
+# # Iterate over the patterns and execute the Python script
+# for pattern in "${patterns[@]}"; do
+#     set -- $pattern
+#     pdb_file="${1}_crosslink_list_interprotein_crosslinks.csv"
+#     python ~/EASAL/scripts/2_getting_random_xlinks.py "$pdb_file" "$2" "$3"
+# done
