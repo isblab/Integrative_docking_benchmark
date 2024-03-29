d3cd382afba9c810bc5f6384488f20cec911a18a
Shruthi Viswanath <shruthivis@users.noreply.github.com>
2024-03-29 14:45:11 +0530

    Update calc_xlink_dist_perc_pdb.py with code comments

--- scripts/crosslink_distance_perc_satisfied_calculation/easal_output/calc_xlink_dist_perc_pdb.py
+++ scripts/crosslink_distance_perc_satisfied_calculation/easal_output/calc_xlink_dist_perc_pdb.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 
 def calculate_distance(structure, model_A, model_B, residue1, residue2):
+    #TODO structure argument is never used 
 
     ca_atom1 = model_A[residue1]['CA']
     ca_atom2 = model_B[residue2]['CA']
@@ -17,6 +18,7 @@ def process_csv(pdb_file, chain_A, chain_B, xlink_file, output_file):
     model_A = structure[0][chain_A]
     model_B = structure[0][chain_B]
     distances = []
+    #TODO cleanup avg_dist everywhere if not using
     avg_dist = []
     with open(xlink_file, 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -24,6 +26,9 @@ def process_csv(pdb_file, chain_A, chain_B, xlink_file, output_file):
         for row in reader:
             residue1, residue2 = map(int, (row[0], row[2]))
             distance = calculate_distance(structure, model_A, model_B, residue1, residue2)
+            #TODO can calculate in-line in this function instead of passing structure objects to another function 
+            #TODO this is all we need distance = model_A['CA']-model_B['CA']
+            
             avg_dist.append(distance)
             distances.append(f'{pdb_file} {distance}')
 
@@ -36,13 +41,19 @@ def main():
     chain_A, chain_B, xlink_file = sys.argv[1:4]
     xlink_filename = os.path.splitext(os.path.basename(xlink_file))[0]
     output_file = os.path.join('/home/muskaan/easal/easal_output/crosslink_distances/', f'{xlink_filename}_distances.txt')
+    #TODO rename as output_xl_satisfaction 
     xl_satisfaction = os.path.join('/home/muskaan/easal/easal_output/xl_satisfaction/', f'{xlink_filename}_perc_satisfied.txt')
 
     for pdb_file in os.listdir(os.getcwd()):
         avg_across_xlinks = []
+        #TODO avg_across is not used
+        
         if pdb_file.endswith(".pdb"):
-            xl_satisfied = pdb_file.split('_')[2]
+            xl_satisfied = pdb_file.split('_')[2] #TODO comment that EASAL outputs a binary string denoting "1" for satisfied and "0" for unsatisfied xlinks. 
+            #TODO non-trivial things like this need to be commented, or else a new person will get confused. 
+            
             avg_dist = process_csv(pdb_file, chain_A, chain_B, xlink_file, output_file)
+            #TODO where is the avg_dist used here? 
 
             perc = 0
             for xl in xl_satisfied:
